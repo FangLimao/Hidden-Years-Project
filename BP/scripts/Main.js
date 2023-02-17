1601898b8c5798ef66503d78bd0ff97e2ce03617
HiddenYear <mucigames@outlook.com>
2023-02-17 14:51:22 +0800

    照着官方示例做了一个脚本UI

--- BP/scripts/Main.js
+++ BP/scripts/Main.js
@@ -3,8 +3,16 @@ import {
   MessageFormData,
   ModalFormData
 } from "@minecraft/server-ui";
-
-let about_ui = new ActionFormData();
-about_ui.title("关于")
-about_ui.button("确定")
-about_ui.show(player)
+const form = new ActionFormData()
+  .title("Months")
+  .body("Choose your favorite month!")
+  .button("January")
+  .button("February")
+  .button("March")
+  .button("April")
+  .button("May");
+form.show(players[0]).then((response) => {
+  if (response.selection === 3) {
+    dimension.runCommand("say I like April too!");
+  }
+});
