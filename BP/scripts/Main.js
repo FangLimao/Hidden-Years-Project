76e47d59ac14d46226ad9e076f666f605b83b2d6
HiddenYear <mucigames@outlook.com>
2023-02-17 11:30:47 +0800

    添加方块：幻象方块-末地石

--- BP/scripts/Main.js
+++ BP/scripts/Main.js
@@ -3,10 +3,8 @@ import {
   MessageFormData,
   ModalFormData
 } from "@minecraft/server-ui";
-function aboutUI(){
-	/*UI界面 */
+
 let about_ui = new ActionFormData();
 about_ui.title("关于")
 about_ui.button("确定")
 about_ui.show(player)
-}
