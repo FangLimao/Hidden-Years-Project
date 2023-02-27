b7f8df69d21c430499fa1540d5a4492670ca0411
HiddenYear <mucigames@outlook.com>
2023-02-27 13:58:27 +0800

    将项目从vs code迁移至bridge v2

---
+++ BP/scripts/Main.js
@@ -0,0 +1,20 @@
+/* 就只是一个测试
+import {
+  ActionFormData,
+  MessageFormData,
+  ModalFormData
+} from "@minecraft/server-ui";
+const form = new ActionFormData()
+  .title("Months")
+  .body("Choose your favorite month!")
+  .button("January")
+  .button("February")
+  .button("March")
+  .button("April")
+  .button("May");
+form.show(players[0]).then((response) => {
+  if (response.selection === 3) {
+    dimension.runCommand("say I like April too!");
+  }
+});
+*/
